6d2daf96b453c8dbd642f875c8b15a93beeaae52
Evgeniy <kudel89@gmail.com>
2017-08-26 03:51:47 +0300

    Word Press add
    Add word press version

---
+++ test_wp/wp-content/themes/intersog/footer.php
@@ -0,0 +1,172 @@
+<!-- footer -->
+<!-- Secure & Verified -->
+<div class="secure">
+<div class="secure-container">
+	<h3>Secure & Verified</h3>
+
+	<!-- clearfix -->
+	<div class="clearfix"></div>
+
+	<img src="<?php bloginfo('template_directory'); ?>/img/secure-ico-1.png" alt="Norton" class="secure-img-1">
+	<img src="<?php bloginfo('template_directory'); ?>/img/secure-ico-2.png" alt="Trust" class="secure-img-2">
+	<img src="<?php bloginfo('template_directory'); ?>/img/secure-ico-3.png" alt="McAfee" class="secure-img-3">
+	<img src="<?php bloginfo('template_directory'); ?>/img/secure-ico-4.png" alt="Type" class="secure-img-4">
+	<img src="<?php bloginfo('template_directory'); ?>/img/secure-ico-5.png" alt="GSA" class="secure-img-5">
+
+	<!-- clearfix -->
+	<div class="clearfix"></div>
+
+	<h3>Privacy Protected</h3>
+
+	<p class="secure-text">
+		Security is a top priority at SecureUSB. When you submit sensitive information via the website, your information is protected both online and offline.
+		<br />
+		Our Privacy Policy states that we will only share your personal infromation with third parties working on your behalf to complete your orderr such a UPS and FedEx.
+	</p>
+
+</div>
+</div>
+
+
+
+<!-- clearfix -->
+<div class="clearfix"></div>
+
+
+
+<!-- proposal -->
+<div class="proposal">
+<div class="proposal-container">
+	<div class="proposal-block">
+		<div class="proposal-img-box">
+			<img src="<?php bloginfo('template_directory'); ?>/img/proposal-ico-1.png" alt="" class="proposal-ico">
+		</div>
+		<p class="proposal-text-top">Plans & Pricing</p>
+		<p class="proposal-text-bottom">
+			Select the plan that fits your needs. Prices for every budget
+		</p>
+		<a href="" class="proposal-btn">Get Started</a>
+	</div>
+	<div class="proposal-block">
+		<div class="proposal-img-box">
+			<img src="<?php bloginfo('template_directory'); ?>/img/proposal-ico-2.png" alt="" class="proposal-ico">
+		</div>
+		<p class="proposal-text-top">Need more info? Call Us</p>
+		<p class="proposal-text-bottom">
+			Jur expect customer service team is always ready to answer your questions
+		</p>
+		<a href="tel:18008753230" target="_blank" class="proposal-btn">1-800-875-3230</a>
+	</div>
+	<div class="proposal-block">
+		<div class="proposal-img-box">
+			<img src="<?php bloginfo('template_directory'); ?>/img/proposal-ico-3.png" alt="" class="proposal-ico">
+		</div>
+		<p class="proposal-text-top">Start Free Trial Version</p>
+		<p class="proposal-text-bottom">
+			Learn more about SecureBackup with 30-day trial download to better protect your data
+		</p>
+		<a href="" class="proposal-btn">Start Trial</a>
+	</div>
+</div>
+</div>
+
+
+
+<!-- clearfix -->
+<div class="clearfix"></div>
+
+<footer>
+	<div class="footer-social">
+		<a href="" target="_blank" class="social-btn social-fb"></a>
+		<a href="" target="_blank" class="social-btn social-tw"></a>
+		<a href="" target="_blank" class="social-btn social-in"></a>
+		<a href="" target="_blank" class="social-btn social-gl"></a>
+	</div>
+
+	<!-- clearfix -->
+	<div class="clearfix"></div>
+
+	<div class="footer-navigation">
+		<div class="footer-links-holder footer-links-one">
+			<h4><a href="wordpress/home/" class="footer-links-top">HOME</a></h4>
+			<ul class="footer-links">
+				<li><a href="">Solutions</a></li>
+				<li><a href="">Get User Data</a></li>
+			</ul>
+		</div>
+
+		<div class="footer-links-holder footer-links-two">
+			<h4><a href="wordpress/features/" class="footer-links-top">FEATURES</a></h4>
+			<ul class="footer-links">
+				<li><a href="">Backup</a></li>
+				<li><a href="">Restore</a></li>
+				<li><a href="">Security & Protection</a></li>
+				<li><a href="">Advanced Settings</a></li>
+				<li><a href="">Web Portal</a></li>
+			</ul>
+		</div>
+
+		<div class="footer-links-holder footer-links-three">
+			<h4><a href="wordpress/plans-pricing/" class="footer-links-top">PRICING & PLANS</a></h4>
+			<ul class="footer-links">
+				<li><a href="">Personal</a></li>
+				<li><a href="">Family</a></li>
+				<li><a href="">Business</a></li>
+			</ul>
+		</div>
+
+		<div class="footer-links-holder footer-links-four">
+			<h4><a href="" class="footer-links-top">RELATED PRODUCTS</a></h4>
+			<ul class="footer-links">
+				<li><a href="">Secure USB</a></li>
+				<li><a href="">Secure Card</a></li>
+			</ul>
+		</div>
+
+		<div class="footer-links-holder footer-links-five">
+			<h4><a href="" class="footer-links-top">COMPANY</a></h4>
+			<ul class="footer-links">
+				<li><a href="wordpress/about/">About</a></li>
+				<li><a href="wordpress/certifications/">Certifications</a></li>
+				<li><a href="wordpress/contact-us/">Contact Us</a></li>
+			</ul>
+		</div>
+
+		<div class="footer-links-holder footer-links-six">
+			<h4><a href="" class="footer-links-top">SUPPORT</a></h4>
+			<ul class="footer-links">
+				<li><a href="wordpress/how-it-works/">How It Works</a></li>
+				<li><a href="wordpress/faq/">FAQ</a></li>
+				<li><a href="wordpress/site-map/">Site Map</a></li>
+			</ul>
+		</div>
+	</div>
+
+	<!-- clearfix -->
+	<div class="clearfix"></div>
+
+	<div class="footer-copyright">
+		<p class="footer-text">Copyright 2016 SomeCompany, Inc.</p>
+	</div>
+</footer>
+
+
+
+	<div class="hidden"></div>
+
+	<!--[if lt IE 9]>
+	<script src="<?php bloginfo('template_directory'); ?>/libs/html5shiv/es5-shim.min.js"></script>
+	<script src="<?php bloginfo('template_directory'); ?>/libs/html5shiv/html5shiv.min.js"></script>
+	<script src="<?php bloginfo('template_directory'); ?>/libs/html5shiv/html5shiv-printshiv.min.js"></script>
+	<script src="<?php bloginfo('template_directory'); ?>/libs/respond/respond.min.js"></script>
+	<![endif]-->
+
+	<!-- Load Scripts -->
+	<script src="<?php bloginfo('template_directory'); ?>/libs/modernizr/modernizr.js"></script>
+	<script src="<?php bloginfo('template_directory'); ?>/libs/respond/respond.min.js"></script>
+	<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+	<script src="<?php bloginfo('template_directory'); ?>/libs/bootstrap/js/bootstrap.min.js"></script>
+	<script src="<?php bloginfo('template_directory'); ?>/js/common.js"></script>
+
+</body>
+</html>
---
+++ test_wp/wp-content/themes/intersog/header.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="ru"> <!--<![endif]-->
+
+<head>
+
+	<meta charset="utf-8">
+
+	<title>Test</title>
+	<meta name="description" content="">
+
+	<!-- Load CSS -->
+
+	<!-- normalize.css v7.0.0 -->
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/normalize.css">
+
+	<!-- Bootstrap 3.3.7 -->
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/libs/bootstrap/css/bootstrap.min.css">
+
+	<!-- FONT: 'Open Sans' -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600,700" rel="stylesheet">
+
+	<!-- CSS Styles: Main -->
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/main.css">
+	<!-- CSS Styles: Media -->
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/media.css">
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/img/favicon/favicon.ico" type="image/x-icon">
+	<link rel="apple-touch-icon" href="<?php bloginfo('template_directory'); ?>/img/favicon/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="<?php bloginfo('template_directory'); ?>/img/favicon/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo('template_directory'); ?>/img/favicon/apple-touch-icon-114x114.png">
+
+</head>
+
+<body>
+
+<header>
+	<!-- underhead -->
+	<section class="underhead-cover">
+	<div class="underhead-container">
+		<a href="tel:18008753230" target="_blank">1-800-875-3230</a>
+		<img src="<?php bloginfo('template_directory'); ?>/img/header-tel-ico.png" alt="1-800-875-3230" class="header-tel-ico">
+		<a href="mailto:sales@somecompany.com" target="_blank">sales@somecompany.com</a>
+		<img src="<?php bloginfo('template_directory'); ?>/img/header-mail-ico.png" alt="sales@somecompany.com" class="header-mail-ico">
+	</div>
+	</section>
+	<!-- / underhead -->
+
+	<!-- clearfix -->
+	<div class="clearfix"></div>
+
+	<!-- NAV -->
+	<nav class="navbar">
+		<div class="navbar-header keep-open">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar2">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="<?php bloginfo('home'); ?>" class="logo"><img src="<?php bloginfo('template_directory'); ?>/img/logo.png" alt="Logo" class="img-responsive"></a>
+		</div>
+		<div id="navbar2" class="navbar-collapse collapse">
+			<?php    /**
+				* Displays a navigation menu
+				* @param array $args Arguments
+				*/
+				$args = array(
+					'theme_location' => '',
+					'menu' => '',
+					'container' => 'ul',
+					'container_class' => 'nav navbar-nav navbar-right',
+					'container_id' => '',
+					'menu_class' => 'nav navbar-nav navbar-right',
+					'menu_id' => '',
+					'echo' => true,
+					'fallback_cb' => 'wp_page_menu',
+					'before' => '',
+					'after' => '',
+					'link_before' => '',
+					'link_after' => '',
+					'items_wrap' => '<ul id = "%1$s" class = "%2$s">%3$s</ul>',
+					'depth' => 0,
+					'walker' => ''
+				);
+			
+				wp_nav_menu( $args ); ?>
+<!-- NAVBAR standart
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Features</a></li>
+				<li class="active"><a href="#">Plans & Pricing</a></li>
+
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Company <span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="#">About</a></li>
+						<li><a href="#">Certifications</a></li>
+						<li><a href="#">Contact Us</a></li>
+					</ul>
+				</li>
+
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Support <span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="#">How It Works</a></li>
+						<li><a href="#">FAQ</a></li>
+						<li><a href="#">Site Map</a></li>
+					</ul>
+				</li>
+
+				<li class="menu-right"><a href="#">Customer Login</a></li>
+				<div class="vertical-line"></div>
+				<li><a href="#">Free Trial</a></li>
+			</ul>
+ -->
+		</div>
+	</nav>
+	<!-- / NAV -->
+
+	<!-- clearfix -->
+	<div class="clearfix"></div>
+
+	<!-- breadcrumb -->
+	<section class="breadcrumb-cover">
+	<div class="header-container">
+		<a href="<?php bloginfo('home'); ?>"><img src="<?php bloginfo('template_directory'); ?>/img/breadcrumb-ico.png" alt="1-800-875-3230" class="breadcrumb-ico"></a>
+		<img src="<?php bloginfo('template_directory'); ?>/img/breadcrumb-arrow.png" alt="1-800-875-3230" class="breadcrumb-arrow">
+		<a href="<?php $currenturl = get_permalink(); ?>" class="breadcrumb-url"><?php the_title(); ?></a>
+	</div>
+	</section>
+	<!-- / breadcrumb -->
+
+	<!-- clearfix -->
+	<div class="clearfix"></div>
+
+</header>
+
+<!-- heading -->
+<section class="heading-cover">
+<div class="header-container"> 
+	<!-- <h1>Plans & Pricing</h1> -->
+	<h1><?php the_title(); ?></h1>
+</div>
+</section>
+<!-- / heading -->
+
+<!-- clearfix -->
+<div class="clearfix"></div>
