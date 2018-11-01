ee827549c6770209137975b3c8b11309ad8a776d
Evgeniy <kudel89@gmail.com>
2018-11-01 16:03:08 +0200

    Fix for thankyou page

--- tuning/js/common.js
+++ tuning/js/common.js
@@ -7,24 +7,6 @@ $(function () {
 		});
 	};
 
-	//E-mail Ajax Send
-	//Documentation & Example: https://github.com/agragregra/uniMail
-	// $("form").submit(function () { //Change
-	// 	var th = $(this);
-	// 	$.ajax({
-	// 		type: "POST",
-	// 		url: "mail.php", //Change
-	// 		data: th.serialize()
-	// 	}).done(function () {
-	// 		alert("Thank you!");
-	// 		setTimeout(function () {
-	// 			// Done Functions
-	// 			th.trigger("reset");
-	// 		}, 1000);
-	// 	});
-	// 	return false;
-	// });
-
 	//Chrome Smooth Scroll
 	try {
 		$.browserSelector();
@@ -147,6 +129,7 @@ $("#owl-one").owlCarousel({
 	loop: true,
 	items: 1,
 	nav: true,
+	dots: true,
 	margin: 0,
 	autoplay: true,
 	autoplayTimeout: 3000,
